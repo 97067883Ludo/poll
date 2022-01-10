db71ac5f52b7222301e8c195e2c3b4d9342c5641
Ludo Tielbeke <97067883@st.deltion.nl>
2022-01-10 09:33:03 +0100

    fresh new begin

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-require 'user.class.php';
 
 
-$MAC = exec('getmac');
-
-$MAC = strtok($MAC, " ");
-
-$user = new user($MAC);
-
-echo $user->getmac();
 
 ?>
--- user.class.php
+++ user.class.php
@@ -1,16 +1,5 @@
 <?php
 
-class user{
-    
-    public $mac;
 
-    public function __construct($mac){
-        $this->mac = $mac;
-    }
-    public function getmacfromdb(){
-        
-    }
-    public function getmac(){
-        return $this->mac;
-    }
-}
+
+?>
